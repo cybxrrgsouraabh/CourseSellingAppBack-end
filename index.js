b4daf76172e084529f261a6be5c1249e52179e8b
cyybxrg Sourabh <cyborgop6@gmail.com>
2024-08-20 23:29:43 +0530

    Revert "finished with index.js and made user and routes layout"
    This reverts commit 4d0f034db23dc8b1bea68f83ff273ce231a45e1e.

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const express = require("express")
-const adminRouter = require("./routes/admin")
-const userRouter = require("./routes/user")
-const bodyParser = require("body-parser")
 const app = express()
 const port = 3000;
 
-app.use(bodyParser);
-app.use('/user', userRouter);
-app.use("/admin", adminRouter)
 
 app.listen(port, ()=>{
     console.log(`your server runs on port: ${port}`);
